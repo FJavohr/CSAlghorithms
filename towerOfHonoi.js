f72f8af5fe344b920e11d35733e88d7830ad5363
Javokhir Majidov <jonygesssss@gmail.com>
2023-08-01 18:00:27 +0600

    Honoi tower

---
+++ towerOfHonoi.js
@@ -0,0 +1,10 @@
+function towerOfHonoi(n, fromRod, toRod, usingRod) {
+  if (n === 1) {
+    console.log(`Move disk 1 from ${fromRod} to ${toRod}`)
+    return
+  }
+  towerOfHonoi(n - 1, fromRod, usingRod, toRod)
+  console.log(`Move disk ${n} from ${fromRod} to ${toRod}`);
+  towerOfHonoi(n - 1, usingRod, toRod, fromRod)
+}
+towerOfHonoi(5, 'A', 'B', 'C')
